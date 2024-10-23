3f9e378309ea2dc3cb844bbbaa68a9f7a4f0fbb0
Lizandra Silva <127451571+maopequena@users.noreply.github.com>
2024-10-23 04:08:15 -0300

    .

--- src/js/tests.js
+++ src/js/tests.js
@@ -1,3 +1,10 @@
 const randomBoolean = () => Math.random() >= 0.5;
 
-console.log(randomBoolean()); // true;
+console.log(randomBoolean());
+
+function isAdult(age) {
+	if (age < 18) text = "Too young to buy alcohol";
+	else text = "Adult";
+}
+
+console.log(isAdult(17));
