9051d8a346378d3d69b8123fa62ed077f7df9edf
Lizandra Silva <127451571+maopequena@users.noreply.github.com>
2024-11-07 12:46:12 -0300

    .

--- src/js/tests.js
+++ src/js/tests.js
@@ -2,9 +2,18 @@ const randomBoolean = () => Math.random() >= 0.5;
 
 console.log(randomBoolean());
 
-function isAdult(age) {
-	if (age < 18) text = "Too young to buy alcohol";
-	else text = "Adult";
-}
+x = 123;
 
-console.log(isAdult(17));
+var1 = String(x);
+var2 = String(123);
+var3 = String(100 + 23);
+var4 = x.toString();
+var5 = (123).toString();
+var6 = (100 + 23).toString();
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+console.log(var4);
+console.log(var5);
+console.log(var6);
