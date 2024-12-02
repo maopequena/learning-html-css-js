8dc193978fd62360a5d9b1602924f8a394bf884c
Lizandra Silva <127451571+maopequena@users.noreply.github.com>
2024-12-02 13:40:10 -0300

    .

--- src/js/tests.js
+++ src/js/tests.js
@@ -2,18 +2,6 @@ const randomBoolean = () => Math.random() >= 0.5;
 
 console.log(randomBoolean());
 
-x = 123;
+const obj = /e/.exec("The best things in life are free!");
 
-var1 = String(x);
-var2 = String(123);
-var3 = String(100 + 23);
-var4 = x.toString();
-var5 = (123).toString();
-var6 = (100 + 23).toString();
-
-console.log(var1);
-console.log(var2);
-console.log(var3);
-console.log(var4);
-console.log(var5);
-console.log(var6);
+console.log(obj);
